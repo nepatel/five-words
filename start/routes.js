2ad3506461ca305ac4d771aaf3ebb53dbf028faa
Neil Patel <nepatel@davidson.edu>
2019-08-22 12:42:29 -0700

    first commit

---
+++ start/routes.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+// landing page
+Route.on("/").render("index");
+
+// login page
+Route.on("/login").render("auth.login");
+Route.post("/login", "UserController.login").validator("LoginUser");
+
+//sign up
+Route.on("/signup").render("auth.signup");
+Route.post("/signup", "UserController.create").validator("CreateUser");
+
+//logout
+Route.get("/logout", async ({ auth, response }) => {
+  await auth.logout();
+  return response.redirect("/");
+});
+
+// directory page
+Route.on("/directory").render("directory");
+
+// input page
+Route.get("/journal", "JournalController.index");
+Route.post("/journal", "JournalController.store");
+
+// data page
+Route.on("/reflection").render("dataVis");
